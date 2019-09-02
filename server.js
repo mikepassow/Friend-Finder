940f1597b0b98f6fd2da690caceebc4c1aa4df9d
Michael Passow <passowmichael@yahoo.com>
2019-09-02 17:13:28 -0400

    added images/people info

--- server.js
+++ server.js
@@ -2,22 +2,22 @@
 
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
 var PORT = process.env.PORT || 8080;
 
 var jsonParser = bodyParser.json();
 
-var urlencodedParser = bodyParser.urlencoded ({extended: false })
+var urlencodedParser = bodyParser.urlencoded ({extended: false });
 
 // parse various different custom JSON types as JSON
-app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.json({ type: 'application/*+json' }));
  
 // parse some custom thing into a Buffer
-app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
  
 // parse an HTML body into a string
-app.use(bodyParser.text({ type: 'text/html' }))
+app.use(bodyParser.text({ type: 'text/html' }));
 
 require("./app/routing/html-routes.js")(app);
 
