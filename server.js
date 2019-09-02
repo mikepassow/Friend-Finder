455c923b3c039cfade525d6593630499ad2bfe58
Michael Passow <passowmichael@yahoo.com>
2019-09-02 15:46:27 -0400

    added paths

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
 // parse an HTML body into a string
 app.use(bodyParser.text({ type: 'text/html' }))
 
+require("./app/routing/html-routes.js")(app);
+
 app.listen(PORT, function() {
     console.log('App listening on PORT: ' + PORT)
 });
