50342f4d468c085b49b0829c160a4d1b6963e0ba
Michael Passow <passowmichael@yahoo.com>
2019-10-23 11:25:11 -0400

    updated server.js

--- server.js
+++ server.js
@@ -6,20 +6,33 @@ var bodyParser = require('body-parser');
 
 var PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded ({extended: true }));
+// create application/json parser
+// var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // parse various different custom JSON types as JSON
-app.use(bodyParser.json({ type: 'application/*+json' }));
- 
+app.use(bodyParser.json({
+    type: 'application/*+json'
+}))
+
 // parse some custom thing into a Buffer
-app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
- 
+app.use(bodyParser.raw({
+    type: 'application/vnd.custom-type'
+}))
+
 // parse an HTML body into a string
-app.use(bodyParser.text({ type: 'text/html' }));
+app.use(bodyParser.text({
+    type: 'text/html'
+}))
 
-require("./friendfinder/app/routing/api-routes.js")(app);
-require("./friendfinder/app/routing/html-routes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
-app.listen(PORT, function() {
-    console.log('App listening on PORT: ' + PORT)
+app.listen(PORT, function () {
+    console.log("App listenting on PORT " + PORT);
+
 });
